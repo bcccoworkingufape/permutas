37af636b0373ad8d5dd9dcebed68eb0b75cece1d
Gabriel Castro <gabrielcssg@gmail.com>
2021-06-10 22:46:31 -0300

    timeout

--- permutaservidor_app/src/services/api.js
+++ permutaservidor_app/src/services/api.js
@@ -3,7 +3,9 @@ import Axios from 'axios';
 const api = Axios.create({
   // baseURL: 'http://localhost:3333',
   // baseURL: 'http://192.168.1.11:3333'
-  baseURL: 'https://permutas.rj.r.appspot.com/'
+  baseURL: 'https://permutas.rj.r.appspot.com/',
+  timeout: 9999,
+  timeoutErrorMessage: "Falha de conexão",
 });
 
 export default api;
